8a8bf955d8ff35200df94e858a5d33d1664e7363
Judah Hunger <judah.hunger@gmail.com>
2018-01-25 17:42:00 -0800

    working on dropdown

--- JS/app.js
+++ JS/app.js
@@ -1,17 +1,7 @@
-function showProducts(){
-  document.getElementById("myProducts").classList.toggle('show');
-}
+function showImage(){
+  var selector = document.getElementById('main-products');
+  var selected = selector.selectedIndex;
+  document.getElementById('target-image').innerHTML = selector.options[selected].text;
 
-window.onclick = function(event){
-  if (!event.target.matches('.dropbtn')){
-
-    var dropdowns = document.getElementsByClassName('dropdown-content');
-    var i;
-    for (i = 0; i < dropdowns.length; i++){
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')){
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
 }
+showImage();
